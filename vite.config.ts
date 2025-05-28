80299f84658ce26a335351610b2f8e0976185bce
Soham Pagi <paguisoham36@gamail.com>
2025-05-29 00:40:32 +0530

    add gh pages settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/product-list-with-cart/",
   plugins: [react(), tailwindcss()],
 })
