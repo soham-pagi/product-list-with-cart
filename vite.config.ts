d656026c6ed7e8a42e45c065923bfd330756ed65
Soham Pagi <paguisoham36@gamail.com>
2025-05-29 01:24:04 +0530

    edit base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/product-list-with-cart/",
+  base: "/product-list-with-cart",
   plugins: [react(), tailwindcss()],
 })
